5f6e83279c2b0cfc758a33c59da2c4500c57eff4
Jacek Bajor <jacek.m.bajor@vanderbilt.edu>
2016-11-14 10:49:10 -0600

    Initial commit - project template

---
+++ README.md
@@ -0,0 +1,33 @@
+fast-intensity
+===============================
+
+version number: 0.0.1
+author: Jacek Bajor
+
+Overview
+--------
+
+Fast density inference
+
+Installation / Usage
+--------------------
+
+To install use pip:
+
+    $ pip install fast-intensity
+
+
+Or clone the repo:
+
+    $ git clone https://github.com/jbajor/fast-intensity.git
+    $ python setup.py install
+    
+Contributing
+------------
+
+TBD
+
+Example
+-------
+
+TBD
---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+__version__ = '0.0.1'
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# get the dependencies and installs
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
+
+install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
+dependency_links = [x.strip().replace('git+', '') for x in all_reqs if x.startswith('git+')]
+
+setup(
+    name='fast-intensity',
+    version=__version__,
+    description='Fast density inference',
+    long_description=long_description,
+    url='https://github.com/jbajor/fast-intensity',
+    download_url='https://github.com/jbajor/fast-intensity/tarball/' + __version__,
+    license='BSD',
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Developers',
+      'Programming Language :: Python :: 3',
+    ],
+    keywords='',
+    packages=find_packages(exclude=['docs', 'tests*']),
+    include_package_data=True,
+    author='Jacek Bajor',
+    install_requires=install_requires,
+    dependency_links=dependency_links,
+    author_email='jacek.m.bajor@vanderbilt.edu'
+)
