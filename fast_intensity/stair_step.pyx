5ec7328cd278af3b0226f051cd465ae8107b817d
Jacek Bajor <jacek.m.bajor@vanderbilt.edu>
2016-12-14 10:51:21 -0600

    Updated stair_step, added docstring

--- fast_intensity/stair_step.pyx
+++ fast_intensity/stair_step.pyx
@@ -2,13 +2,26 @@ import numpy as np
 cimport numpy as np
 
 def stair_step(x, y, xp, yp):
+    """
+    Previous neighbor interpolation.
+
+    Args:
+        x (list or np.array of numbers): sample points
+        y (list or np.array of numbers): sample values (same size as x)
+        xp (list or np.array of numbers): query points
+        yp (list or np.array of numbers): preallocated list or np.array for
+            query values (same size as xp)
+
+    Returns:
+        np.array of interpolated values (float)
+    """
     cdef int n = len(xp)
     cdef int m = len(y)
     cdef int j = 0
     cdef int i = 0
 
-    while xp[j] < x[i] and j < n:
-        yp[j] = 0
+    while j < n and xp[j] < x[i]:
+        yp[j] = np.nan
         j += 1
 
     while j < n and i < m:
