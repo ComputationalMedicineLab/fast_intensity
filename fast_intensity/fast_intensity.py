3cf705d4091b913277b4cc8a892f0aac26f06a74
Jacek Bajor <jacek.m.bajor@vanderbilt.edu>
2017-02-10 14:04:21 -0600

    FastIntensity now derived from FastBase

--- fast_intensity/fast_intensity.py
+++ fast_intensity/fast_intensity.py
@@ -1,3 +1,5 @@
+from .fast_base import FastBase
+
 from datetime import datetime
 
 import numpy as np
@@ -11,7 +13,7 @@ from .stair_step import stair_step
 from .fast_hist import fast_hist
 
 
-class FastIntensity(object):
+class FastIntensity(FastBase):
     """Estimates (potentially nonstationary) event intensity vs. time.
 
     This class uses Completely Random Average Shifted Histograms (CRASH) to
@@ -86,7 +88,7 @@ class FastIntensity(object):
             start_date (date/datetime)
             end_date (date/datetime)
         """
-        events, start_e, end_e = FastIntensity.convert_dates_to_events(dates,
+        events, start_e, end_e = FastBase.convert_dates_to_events(dates,
                                     start_date, end_date)
 
         return cls(events, start_e, end_e)
@@ -107,59 +109,10 @@ class FastIntensity(object):
             date_format (string): format of dates in the input (same as used
                 in datetime.datetime.strptime() function)
         """
-        start_date = datetime.strptime(start_date, date_format)
-        end_date = datetime.strptime(end_date, date_format)
-        dates = [ datetime.strptime(d, date_format) for d in dates]
-        events, start_e, end_e = FastIntensity.convert_dates_to_events(dates,
-                                    start_date, end_date)
+        events, start_e, end_e = FastBase.convert_string_dates_to_events(dates,
+                                    start_date, end_date, date_format)
 
         return cls(events, start_e, end_e)
-
-    @staticmethod
-    def time_delta_in_days(a, b):
-        """
-        Return time difference in days.
-
-        Args:
-            a, b (date or datetime)
-
-        Returns:
-            float: time difference in days (exact, not rounded)
-        """
-        return (a-b).total_seconds()/(24*60*60)
-
-    @staticmethod
-    def convert_dates_to_events(dates, start_date, end_date):
-        """
-        Convert dates to events.
-
-        Args:
-            dates (array-like of date/datetime)
-            start_date (date/datetime)
-
-        Returns:
-            list of numbers representing events
-        """
-        events = np.zeros(len(dates))
-        for i, d in enumerate(dates):
-            events[i] = FastIntensity.time_delta_in_days(d, start_date)
-        return events, 0, FastIntensity.time_delta_in_days(end_date, start_date)
-
-    def _generate_grid(self, resolution, density):
-        """
-        Generate grid for intensity (x-axis).
-
-        Args:
-            density (number): average number of bin edges between neighboring
-                points (default 1/365).
-            resolution (number): resolution for bin edges in units of days
-                (default 1).
-
-        Returns:
-            np.array of evenly spaced numerical values
-        """
-        grid_len = int(np.round((self.end - self.start) / resolution))
-        return np.linspace(self.start, self.end, grid_len)
 
     def run_inference(self, density=0.00274, resolution=1, iterations=100):
         """
