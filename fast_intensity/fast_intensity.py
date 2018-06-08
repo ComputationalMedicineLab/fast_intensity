535d363c16b108e1bd16831344c57594864bd013
Tom Lasko <tom.lasko@vanderbilt.edu>
2018-06-08 09:51:42 -0500

    Fixed pathologic behavior at extremes.
    The algorithm had allowed for arbitrarily high intensities around events that
    occured at or near the endpoints of the time interval investigated, and tended to
    give spiky intensity behavior there. Changed the algorithm to require a minimum set
    of events per bin (default 3, counting range endpoints), which removes this
    problem, and places reasonable constraints on the maximum intensity estimated
    in the interior of the range.
    
    Also, re-coded the computation to be slightly more efficient and (hopefully) a
    little clearer.

--- fast_intensity/fast_intensity.py
+++ fast_intensity/fast_intensity.py
@@ -23,21 +23,20 @@ class FastIntensity(FastBase):
     compute a continuous curve of event  intensity vs. time, as described in **
     Citation TBD **.
 
-    Each histogram is defined by a random number of bin edges, with the location
-    of each bin edge  sampled uniformly at random between event *indices* (not
-    their locations). For example, with the sequence of events  [1, 2, 100],
-    there is the same probability that an edge will appear between 1 and 2 as
-    between 2 and 100.  This allows for the final density estimation to adapt
-    its 'bandwidth' to the nonstationarity of event locations.  The smoothness
-    and resolution of the resulting curves can be set.
+    Each histogram is defined by a random number of bin edges, with the
+    location of each bin edge sampled uniformly at random between event
+    *indices* (not their locations). For example, with the sequence of events
+    [1, 2, 3, 100], there is the same probability that an edge will appear between
+    3 and 3 as between 3 and 100.  This allows for the final density estimation
+    to adapt its bandwidth to the nonstationarity of event locations. A
+    constraint on the minimum number of events per bin keeps density peaks from
+    forming pathologically around each event and at endpoints.
 
     Attributes:
-        events (array-like of real numbers): event times in units of days
-            since an arbitrary reference point
-        start (number): beginning of the computed inference time range
-        end (number): end of the computed inference time range
-        grid (np.array of real numbers): evenly spaced timepoints at which the
-            curve is computed
+        events (array-like of sorted real numbers): event times
+        grid (np.array of sorted real numbers): timepoints at which the
+            intensity curve is computed
+        min_count: The minimum number of points per bin.
 
     Usage:
         events = [10, 15, 16, 17, 28]
@@ -45,140 +44,129 @@ class FastIntensity(FastBase):
 
         fi = FastIntensity(events, grid=grid)
         intensity = fi.run_inference()
-
-        # dates = [dt.datetime(2000, 1, 2), dt.datetime(2000, 1, 10),
-        #          dt.datetime(2000, 1, 15), dt.datetime(2000, 2, 1)]
-
-        # fi = FastIntensity.from_dates(dates, start_date=dt.datetime(2000, 1, 1),
-        #                               end_date=dt.datetime(2000, 3, 1))
-        # intensity = fi.run_inference()
-
-        # date_strs = ['2000-01-02', '2000-01-10', '2000-01-15', '2000-02-01']
-
-        # fi = FastIntensity.from_string_dates(date_strs, start_date='2000-01-01',
-        #                                      end_date='2000-03-01',
-        #                                      date_format='%Y-%m-%d')
-        # intensity = fi.run_inference()
     """
 
-    def __init__(self, events, grid, density=0.00274, iterations=100):
+    def __init__(self, events, grid, iterations=100, min_count=3):
         """
         Initialize with events and inference parameters.
 
         Args:
-            events (array-like of real numbers): event times in units of days
-                since an arbitrary reference point
-            grid (array-like of reals): Evenly spaced time points at which to
-                compute the curve.
-            density (number): average number of bin edges between neighboring
-                points for inference (default 1/365).
-            iterations (int): number of inference iterations (default: 100)
+            events (array-like of sorted reals): event times
+            grid (array-like of sorted reals): time points at which to
+                compute the intensity curve.
+            iterations: number of inference iterations (default 100).
+            min_count: The minimum number of events per bin (default 3).
         """
 
         super().__init__(events, grid)
-
-        self.density = density
         self.iterations = iterations
 
-    # @classmethod
-    # def from_dates(cls, dates, start_date, end_date, density=0.00274,
-    #                resolution=1, iterations=100):
-    #     """
-    #     Convert dates/datetimes to events and initialize an instance.
-
-    #     Args:
-    #         dates (array-like of date/datetime)
-    #         start_date (date/datetime)
-    #         end_date (date/datetime)
-    #         density (number): average number of bin edges between neighboring
-    #             points for inference (default 1/365).
-    #         resolution (number): resolution for bin edges in units of days
-    #             for inference (default 1).
-    #         iterations (int): number of inference iterations (default: 100)"""
-    #     events, start_e, end_e = FastBase.convert_dates_to_events(
-    #         dates, start_date, end_date)
-    #     return cls(events, start_e, end_e, density, resolution, iterations)
-
-    # @classmethod
-    # def from_string_dates(cls, dates, start_date, end_date,
-    #                       date_format='%Y-%m-%d %H:%M:%S', density=0.00274,
-    #                       resolution=1, iterations=100):
-    #     """
-    #     Convert date strings to events and initialize an instance.
-
-    #     Args:
-    #         dates (array-like of strings): dates represented by correctly
-    #             formatted strings
-    #         start_date (string): date represented by a correctly formatted
-    #             string
-    #         end_date (string): date represented by a correctly formatted
-    #             string
-    #         date_format (string): format of dates in the input (same as used
-    #             in datetime.datetime.strptime() function)
-    #         density (number): average number of bin edges between neighboring
-    #             points for inference (default 1/365).
-    #         resolution (number): resolution for bin edges in units of days
-    #             for inference (default 1).
-    #         iterations (int): number of inference iterations (default: 100)
-    #      """
-    #     events, start_e, end_e = FastBase.convert_string_dates_to_events(
-    #         dates, start_date, end_date, date_format)
-    #     return cls(events, start_e, end_e, density, resolution, iterations)
-
     def run_inference(self):
-        """
-        Run event intensity inference.
+        """Run event intensity inference.
 
-        Args:
-
-        Returns
-            np.array of event intensity
+        Returns:
+            np.array of event intensity, calculated at times defined by
+              self.grid, with units of events per time.
         """
         meanvals = np.zeros(len(self.grid))
         vals = np.zeros(len(self.grid), dtype=np.float)
         n = len(self.events) + 1
-        num_edges = int(2 + np.max([2, np.ceil(self.density * n)]))
+        min_count = 3
 
-        # compute event_indices once for all iterations of randomize_edges, for
-        # efficiency
-        event_indices = np.linspace(0, n, n + 1)
+        # Compute event_indices once for all iterations of _get_boundaries, for
+        # efficiency. (This has a measurable effect on run time.)
+        self.event_indices = np.linspace(0, n, n + 1)
 
         self._events_w_endpoints = np.concatenate(([self.start], self.events,
                                                    [self.end]))
+        max_bins = int(self.event_indices[-1] // min_count)
+
         for i in range(self.iterations):
-            edges = self.randomize_edges(
-                num_edges, self.density, self.resolution, event_indices)
-            h = fast_hist(self.events, edges)
-            vals = stair_step(edges, h, self.grid, vals)
-            # vals = self._smooth_interp(edges, h, self.grid)
+            if max_bins < 2:
+                num_bins = 1
+            else:
+                # randint high value is exclusive
+                num_bins = npr.randint(1, max_bins + 1)
+
+            boundaries = self._get_boundaries(num_bins, min_count)
+            h = fast_hist(self.events, boundaries)
+            vals = stair_step(boundaries, h, self.grid, vals)
             meanvals = meanvals + (vals - meanvals) / (i + 1)
+
         return meanvals
 
-    def randomize_edges(self, num_edges, density, resolution, event_indices):
-        """
-        Randomize bin edges for histogram.
+    def _get_boundaries(self, num_bins, min_count):
+        """Compute random bin boundaries for histogram, respecting min_count.
+
+        Boundaries are sampled uniformly at random in sequence space, with the
+        constraint that all bins have at least min_count events in them
+        (with endpoints considered events). This means, that a boundary is
+        equally likely to occur between any two events, regardless of the
+        spacing of those events, so long as min_count is respected. This
+        tends to give a smoothness to the final density estimation that varies
+        appropriately with the density of events.
+
+        Args:
+            num_bins: The number of bins to be defined by the boundaries.
+            min_count: The minimum number of events (including
+              endpoints) to be present in any bin.
 
         Returns:
-            np.array of new bin edges
+            np.array of new bin boundaries
         """
-        n = len(self.events) + 1
+        sequence_boundaries = _get_sequence_boundaries(
+            num_bins, num_events=len(self.events), min_count=min_count)
 
-        # w is the location of the bins in index space (that is, warped
-        # space).
+        data_boundaries = np.interp(
+            sequence_boundaries, self.event_indices, self._events_w_endpoints)
 
-        w = n * npr.rand(int(np.max([2, np.ceil(density * n)])))
-        w.sort()
+        return data_boundaries
 
-        x = np.zeros(num_edges)
 
-        # endpoints cannot be within the range of events, so use floor and ceil
-        # instead of round
-        x[0] = np.floor(self.start / resolution) * resolution
-        x[-1] = np.ceil(self.end / resolution) * resolution
+def _get_sequence_boundaries(num_bins, num_events, min_count=3):
+    """Compute the bin boundaries in (0-based) sequence index space.
 
-        x[1:-1] = np.interp(w, event_indices, self._events_w_endpoints)
+    For example, a boundary at 0.35 means that the boundary is 35% of the way
+    between the beginning boundary (before any events) and the first
+    event. (Although that particular boundary cannot exist unless min_count=1.)
+    Boundaries are sampled uniformly at random in sequence space, subject to
+    the constraint that all boundaries are separated by at least min_count.
 
-        # y = np.array(y, dtype=np.float)
-        x = np.round(x / resolution) * resolution
+    For efficiency, no checking is done to ensure that the arguments are
+    consistent. Setting num_bins=1 will always return valid boundaries for
+    num_events > 0. Inconsistent combinations as defined below result in
+    undefined behavior.
 
-        return x
+    Args:
+        num_bins: The number of bins to be defined by the boundaries. Must
+          satisfy num_bins <= ((num_events + 2) / min_count) or undefined
+          behavior results.
+        num_events: The positive number of events to be binned (not counting
+          the overall start and end boundaries as events). Must be positive
+          (and nonzero) or underfine behavior results.
+        min_count: The minimum number of indices between boundaries.
+
+    Returns:
+       np.array of bin boundaries.
+    """
+
+    # The bin at each end must contain at least pad = min_count - 1 actual
+    # events, because the endpoints count as an included event, even though the
+    # intervals stop exactly at that event.
+    start = 0
+    end = num_events + 1
+    pad = min_count - 1
+
+    boundaries = np.empty(num_bins + 1, dtype='float')
+    boundaries[0] = start
+    boundaries[-1] = end
+    if num_bins == 1:
+        return boundaries
+
+    boundaries[1:-1] = np.arange(start=pad, stop=min_count * (num_bins - 1),
+                                 step=min_count, dtype='float')
+    slop = npr.uniform(low=0, high=end - pad -
+                       boundaries[-2], size=num_bins - 1)
+    slop.sort()
+    np.add(boundaries[1:-1], slop, out=boundaries[1:-1])
+    return boundaries
