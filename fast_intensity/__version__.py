468145ec793ce3d44eced5ca30dce20222d523c5
John Still <john.m.still@vumc.org>
2018-07-26 14:57:23 -0500

    Bump version

--- fast_intensity/__version__.py
+++ fast_intensity/__version__.py
@@ -4,7 +4,7 @@
 # This file should never import anything from the package, it is instead
 # imported by the package.
 
-__version__ = '1.5.6'
+__version__ = '0.2'
 __version_info__ = tuple(__version__.split('.'))
 
 __author__ = 'Thomas A. Lasko, Jacek Bajor, John M Still'
