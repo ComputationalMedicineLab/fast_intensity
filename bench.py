09530f5fc65eda38556848971218de1ef07e5d0b
John Still <john.m.still@vumc.org>
2019-05-31 15:13:39 -0500

    Time Intensity inference as well as regression

--- bench.py
+++ bench.py
@@ -4,9 +4,6 @@
 Loads some test data, runs a benchmark using ipython magic, and then drops into
 an IPython interactive shell.
 """
-import argparse
-import pathlib
-
 import IPython
 import numpy
 import pyximport
@@ -14,21 +11,19 @@ pyximport.install(setup_args={"include_dirs": numpy.get_include()})
 from fast_intensity import *
 
 if __name__ == '__main__':
-    p = argparse.ArgumentParser()
-    p.add_argument('datfile', type=pathlib.Path)
-    p.add_argument('--repeat', type=int, default=5)
-    p.add_argument('--number', type=int, default=10)
-    args = p.parse_args()
-
-    data = numpy.load(args.datfile)
-    events = data['events']
-    values = data['values']
-    grid = data['grid']
-
     ipython = IPython.get_ipython()
-    magic = 'timeit'
-    setup = (f'-o -r {args.repeat} -n {args.number} '
-              'reg = FastRegression(events, values, grid)')
-    stmt = 'reg.run_inference()'
+    reg_sample = numpy.load('regression.npz')
+    inf_sample = numpy.load('intensity.npz')
 
-    result = ipython.run_cell_magic(magic, setup, stmt)
+    events = reg_sample['events']
+    values = reg_sample['values']
+    grid = reg_sample['grid']
+    setup = f'-o reg = FastRegression(events, values, grid)'
+    stmt = 'reg.run_inference()'
+    reg_result = ipython.run_cell_magic('timeit', setup, stmt)
+
+    events = inf_sample['events']
+    grid = inf_sample['grid']
+    setup = f'-o inf = FastIntensity(events, grid)'
+    stmt = 'inf.run_inference()'
+    inf_result = ipython.run_cell_magic('timeit', setup, stmt)
